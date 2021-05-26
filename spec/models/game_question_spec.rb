2898460f1c52a18eab49d130d262aa6d3cb26e60
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-05-26 21:09:08 +0300

    test for correct_answer_key added

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -34,4 +34,10 @@ RSpec.describe GameQuestion, type: :model do
       expect(game_question.level).to eq(game_question.question.level) 
     end
   end
+
+  context '.correct_answer_key' do
+    it 'returns correct variant' do
+      expect(game_question.correct_answer_key).to eq('b')
+    end
+  end
 end
