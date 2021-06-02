bac2d495503acd06b689f8afea92344866361edf
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-06-02 18:42:23 +0300

    modified according to FactoryBot

--- spec/support/my_spec_helper.rb
+++ spec/support/my_spec_helper.rb
@@ -2,11 +2,15 @@ module MySpecHelper
   # Наш хелпер, для населения базы нужным количеством рандомных вопросов
   def generate_questions(number)
     number.times do
-      FactoryGirl.create(:question)
+      create(:question)
     end
   end
 end
 
 RSpec.configure do |c|
   c.include MySpecHelper
+end
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
 end
