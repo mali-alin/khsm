7e7b546e634b876cdae9dd550fcdcb026497995c
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-06-02 18:28:19 +0300

    test for lifeline 'audience help' added

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -1,5 +1,3 @@
-# (c) goodprogrammer.ru
-
 require 'rails_helper'
 
 # Тестовый сценарий для модели игрового вопроса, в идеале весь наш функционал
@@ -9,7 +7,7 @@ RSpec.describe GameQuestion, type: :model do
   # сценария: она будет создана на фабрике заново для каждого блока it,
   # где она вызывается.
   let(:game_question) do
-    FactoryGirl.create(:game_question, a: 2, b: 1, c: 4, d: 3)
+    create(:game_question, a: 2, b: 1, c: 4, d: 3)
   end
 
   # Группа тестов на игровое состояние объекта вопроса
@@ -38,6 +36,17 @@ RSpec.describe GameQuestion, type: :model do
   context '.correct_answer_key' do
     it 'returns correct variant' do
       expect(game_question.correct_answer_key).to eq('b')
+    end
+  end
+
+  context 'User helpers' do
+    it 'correct audience help' do
+      expect(game_question.help_hash).not_to include(:audience_help)
+
+      game_question.add_audience_help
+
+      expect(game_question.help_hash).to include(:audience_help)
+      expect(game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
     end
   end
 end
