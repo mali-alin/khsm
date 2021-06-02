58997fbb852d58478b62b6f652b5d72cb5b70bce
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-06-02 18:32:38 +0300

    test for audience help added

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  let(:user) { create(:user) }
 
-  let(:user) { FactoryGirl.create(:user) }
+  let(:admin) { create(:user, is_admin: true) } 
 
-  let(:admin) { FactoryGirl.create(:user, is_admin: true) } 
-
-  let(:game_w_questions) { FactoryGirl.create(:game_with_questions, user: user) }
+  let(:game_w_questions) { create(:game_with_questions, user: user) }
 
   context 'Anon' do
-    it 'kick from #show' do
+    it 'kick from #show' do        
       get :show, id: game_w_questions.id
 
       expect(response.status).not_to eq(200)
@@ -58,7 +57,7 @@ RSpec.describe GamesController, type: :controller do
       game = assigns(:game)
 
       #check game status
-      expect(game.finished?).to be_falsey
+      expect(game.finished?).to be false
       expect(game.user).to eq(user)
 
       expect(response).to redirect_to game_path(game)
@@ -69,7 +68,7 @@ RSpec.describe GamesController, type: :controller do
     it '#show game' do
       get :show, id: game_w_questions.id
       game = assigns(:game) 
-      expect(game.finished?).to be_falsey
+      expect(game.finished?).to be false
       expect(game.user).to eq(user)
 
       expect(response.status).to eq(200)
@@ -77,7 +76,7 @@ RSpec.describe GamesController, type: :controller do
     end
 
     it '#show alien game' do
-      alien_game = FactoryGirl.create(:game_with_questions)
+      alien_game = create(:game_with_questions)
 
       get :show, id: alien_game.id
 
@@ -90,10 +89,10 @@ RSpec.describe GamesController, type: :controller do
       put :answer, id: game_w_questions.id, letter: game_w_questions.current_game_question.correct_answer_key
       game = assigns(:game)
 
-      expect(game.finished?).to be_falsey
+      expect(game.finished?).to be false
       expect(game.current_level).to be > 0
       expect(response).to redirect_to(game_path(game))
-      expect(flash.empty?).to be_truthy
+      expect(flash.empty?).to be true
     end
 
     it 'takes money before the end of the game' do
@@ -102,7 +101,7 @@ RSpec.describe GamesController, type: :controller do
       put :take_money, id: game_w_questions.id
       game = assigns(:game)
 
-      expect(game.finished?).to be_truthy
+      expect(game.finished?).to be true
       expect(game.prize).to eq(200)
 
       user.reload
@@ -113,7 +112,7 @@ RSpec.describe GamesController, type: :controller do
     end
 
     it 'try to create second game' do
-      expect(game_w_questions.finished?).to be_falsey
+      expect(game_w_questions.finished?).to be false
 
       post :create
       
@@ -125,17 +124,37 @@ RSpec.describe GamesController, type: :controller do
     end
     
     it 'answers wrong' do
-      wrong_answers = %w[a b c]
+      answers = %w[a b c d]
+      
+      random_wrong_answer = (answers.reject { |answer| answer == 'd' }).sample
 
-      put :answer, id: game_w_questions.id, letter: wrong_answers.sample
+      put :answer, id: game_w_questions.id, letter: random_wrong_answer
 
       game = assigns(:game)
 
       expect(game.status).to eq(:fail)
-      expect(game.finished?).to be_truthy
+      expect(game.finished?).to be true
       expect(game.prize).to eq(0)
       expect(flash[:alert]).to be
       expect(response).to redirect_to(user_path(user))
+    end
+
+    it 'uses audience help' do
+      # Проверяем, что у текущего вопроса нет подсказок
+      expect(game_w_questions.current_game_question.help_hash[:audience_help]).not_to be
+      # И подсказка не использована
+      expect(game_w_questions.audience_help_used).to be false
+    
+      # Пишем запрос в контроллер с нужным типом (put — не создаёт новых сущностей, но что-то меняет)
+      put :help, id: game_w_questions.id, help_type: :audience_help
+      game = assigns(:game)
+    
+      # Проверяем, что игра не закончилась, что флажок установился, и подсказка записалась
+      expect(game.finished?).to be false
+      expect(game.audience_help_used).to be true
+      expect(game.current_game_question.help_hash[:audience_help]).to be
+      expect(game.current_game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
+      expect(response).to redirect_to(game_path(game))
     end
   end
 end
