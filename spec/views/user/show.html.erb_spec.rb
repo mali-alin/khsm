8ca1e365195756f256caa69de5c24f4e165d4377
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-06-07 23:48:28 +0300

    tests for  view users/show added

---
+++ spec/views/user/show.html.erb_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'users/show', type: :view do
+  before(:each) do
+    @user = assign(:user, FactoryBot.create(:user, name: 'Alina'))
+    
+    assign(:games, [FactoryBot.build_stubbed(:game, id: 1, current_level: 5)])
+
+    render
+  end
+  
+  context 'User signed in' do
+    before(:each) do
+      sign_in(@user) 
+
+      render
+    end
+
+    it 'renders user name' do
+      expect(rendered).to match 'Alina'
+    end
+
+    it 'renders change name&password button' do
+      expect(rendered).to match 'Сменить имя и пароль'
+    end
+
+    it 'renders game fragment' do
+      expect(rendered).to match '5'      
+    end
+  end
+
+  context 'Anonymous user' do
+    it 'does not render change name&password button' do
+      expect(rendered).not_to match 'Сменить имя и пароль'
+    end
+  end
+end
